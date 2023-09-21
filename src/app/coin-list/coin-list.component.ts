8ce7d7779c8c0b1e742d4d868f43f80fcd00badb
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-09-21 16:39:36 +0400

    created table

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @Component({
@@ -6,6 +15,41 @@ import { Component } from '@angular/core';
   templateUrl: './coin-list.component.html',
   styleUrls: ['./coin-list.component.scss']
 })
-export class CoinListComponent {
+export class CoinListComponent implements OnInit {
+  constructor(private api: ApiService) {
+  }
+  bannerData: any = [];
 
+
+  displayedColumns: string[] = [`symbol`, `current_price`, `price_change_percentage_24h`, `market_cap`]
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  ngOnInit(): void {
+    this.getBannerData();
+    // this.getTrendingData();
+  }
+
+  getBannerData() {
+    this.api.getAllData().subscribe(res => {
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+  getTrendingData() {
+    this.api.getTrendingCurrency().subscribe(res => console.log(res))
+  }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -8,8 +8,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getAllData(currency: string) {
-    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
+  getAllData() {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`)
   }
   getTrendingCurrency() {
     return this.http.get<any>(`https://api.coingecko.com/api/v3/search/trending`)
