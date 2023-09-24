5762f65b7ecdb2e86f045c3af5b5b48384910070
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-09-24 21:24:23 +0400

    created details page

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-coin-detail',
   templateUrl: './coin-detail.component.html',
   styleUrls: ['./coin-detail.component.scss']
 })
-export class CoinDetailComponent {
+export class CoinDetailComponent implements OnInit {
+
+  coinData: any;
+  coinId!: string;
+  days: number = 1;
+  currency: string = "usd"
+
+
+  constructor(private api: ApiService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    //get :id param
+    this.route.params.subscribe((val) => {
+      this.coinId = val["id"]
+    })
+    this.getCoinData()
+    console.log(this.coinData);
+  }
+
+  getCoinData() {
+    this.api.getCurrencyById(this.coinId).subscribe((res) => { console.log(res); this.coinData = res });
+
+  }
 
 }
